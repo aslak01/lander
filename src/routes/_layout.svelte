172dea840ad67f0c24d7d6fd37e2f321466c86d2
Aslak <aslakrb@gmail.com>
2020-11-05 01:12:45 +0100

    init

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: #FFFCF7;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+		min-height: 100vh;
+	}
+</style>
+
+<!-- <Nav {segment}/> -->
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Elektrisite</title>
+</svelte:head>
+
+
+<h1>Élektrisité</h1>
+<p>Landingsside for en webdev-portfolio.</p>
+
+
+
+
+
+<style>
+	h1 {
+		font-size: 2em;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 2em;
+		}
+	}
+</style>
